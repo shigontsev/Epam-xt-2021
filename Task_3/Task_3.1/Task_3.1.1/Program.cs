d7d22567fbee4c0f3a838c65d55277296eaecde7
Shigontsev Yurii <y.shigontsev@elnavf.ru>
2021-03-22 14:08:56 +0400

    finished Task 3.1.1

---
+++ Task_3/Task_3.1/Task_3.1.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task_3._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("ВЫВОД: Введите N");
+            Survivors survive = new Survivors(InputIntValue());
+            Console.WriteLine("ВЫВОД: Введите, какой по счету человек будет вычеркнут каждый раунд:");
+            survive.Start(InputIntValue());
+            Console.ReadLine();
+        }
+        static int InputIntValue()
+        {            
+            do
+            {
+                Console.Write("ВВОД: ");
+                try
+                {
+                    int result = int.Parse(Console.ReadLine());
+                    if (result < 0)
+                    {
+                        throw new ArgumentNegativeException();
+                    }
+                    if (result == 0)
+                    {
+                        throw new ArgumentNullException();
+                    }
+                    return result;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Не верный формат ввода, повторите попытку.");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Нулевое значение ввода, повторите попытку.");
+                }
+                catch (ArgumentNegativeException)
+                {
+                    Console.WriteLine("Отрицательное значение, повторите попытку.");
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Task_3/Task_3.1/Task_3.1.1/Survivors.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3._1._1
+{
+    public class Survivors
+    {
+        private ArrayList listHuman;
+
+        public int Length => listHuman.Count;
+
+        public Survivors(int n)
+        {
+            CreatListHuman(n);
+        }
+
+        private void CreatListHuman(int n)
+        {
+            listHuman = new ArrayList();
+            for (int i = 1; i <= n; i++)
+            {
+                listHuman.Add($"Human {i}");
+            }
+            Console.WriteLine($"ВЫВОД: Сгенерирован круг людей из {Length} человек");
+        }
+
+        public void Start(int N)
+        {
+            try
+            {
+                int round = 0;
+                while (true)
+                {
+                    OutOfListByMod(N);
+                    round++;
+                    Console.WriteLine($"Раунд {round}. Вычеркнут человек. Людей осталось: {Length}");
+                }
+            }
+            catch (ArgumentNegativeException)
+            {
+                Console.WriteLine("ВЫВОД: Игра окончена. Невозможно вычеркнуть больше людей.");
+            }
+        }
+
+        public void OutOfListByMod(int N)
+        {
+            if (Length < N)
+            {
+                throw new ArgumentNegativeException(nameof(Length));
+            }
+                
+            for (int i = 0; i < Length; i++)
+            {
+                if ((i + 1) % N == 0)
+                {
+                    listHuman[i] = null;
+                }
+            }
+            listHuman.Remove(null);
+        }
+    }
+    public class ArgumentNegativeException : ArgumentOutOfRangeException
+    {
+        public ArgumentNegativeException() : base() { }
+        public ArgumentNegativeException(string paramName) : base(paramName) { }
+        public ArgumentNegativeException(string paramName, string message) : base(paramName, message) { }
+    }
+}
