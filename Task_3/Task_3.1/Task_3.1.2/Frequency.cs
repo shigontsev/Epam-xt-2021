f715c0725098dcc568c1b53473a07eed1fbbbbc7
Shigontsev Yurii <y.shigontsev@elnavf.ru>
2021-03-22 17:11:25 +0400

    create Task_3.1.2

---
+++ Task_3/Task_3.1/Task_3.1.2/Frequency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3._1._2
+{
+    public class Frequency
+    {
+        public string Value { get; set; }
+
+        public int Count { get; set; }
+
+        public Frequency(string value, int count)
+        {
+            Value = value;
+            Count = count;
+        }
+
+        
+
+        //public string value;
+
+        //public int count;
+
+
+    }
+}
---
+++ Task_3/Task_3.1/Task_3.1.2/TextAnalysis.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3._1._2
+{
+    public class TextAnalysis
+    {
+        private List<Frequency> distinctWords = new List<Frequency>();
+
+        private List<string> allWords = new List<string>();
+
+        public TextAnalysis(string text)
+        {
+            allWords.AddRange(ToSplitWords(text));
+            DistinctWords();
+        }
+
+        private string[] ToSplitWords(string text)
+        {
+            StringBuilder sb = new StringBuilder(text.ToLower());
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (Char.IsPunctuation(sb[i]))
+                    sb[i] = ' ';
+            }
+            text = sb.ToString();
+
+            string[] sub_text = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            return sub_text;
+        }
+
+        private void DistinctWords()
+        {
+            List<string> bufferDistWords = new List<string>();
+            foreach (var item in allWords)
+            {
+                if (!bufferDistWords.Contains(item))
+                {
+                    bufferDistWords.Add(item);
+                    int count = allWords.FindAll(x => x == item).Count;
+                    distinctWords.Add(new Frequency(item, count));
+                }
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("List of frequency words:");
+            foreach (var item in distinctWords)
+            {
+                Console.WriteLine($"\"{item.Value}\" = {item.Count}");
+            }
+        }
+        //public void SortByCount()
+        //{
+        //    distinctWords.Sort(Count);
+        //}
+        //public void SortByCount()
+        //{
+        //    distinctWords.Sort();
+        //}
+    }
+}
