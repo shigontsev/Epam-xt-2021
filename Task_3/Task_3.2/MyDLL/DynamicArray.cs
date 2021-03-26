0c7b02d1966cd5efae8d6f9cb3cdcd70a84d6640
Shigontsev Yurii <y.shigontsev@elnavf.ru>
2021-03-26 11:10:36 +0400

    fix older func, add new func Clone, ToArray

--- Task_3/Task_3.2/MyDLL/DynamicArray.cs
+++ Task_3/Task_3.2/MyDLL/DynamicArray.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace MyDLL
 {
-    public class DynamicArray<T>: IEnumerable<T>, IEnumerable
+    public class DynamicArray<T>: IEnumerable<T>, IEnumerable, ICloneable
     {
         private const int size = 8;
 
@@ -26,26 +26,26 @@ namespace MyDLL
             //mainArray = arr.
         }
 
-        //public DynamicArray(T arr)
-        //{
-        //    //mainArray = arr.
-        //}
-
-        public DynamicArray(IEnumerable<T> sourse)
+        public DynamicArray(IEnumerable<T> sourse) : this()
         {
             T[] tempArr = sourse.ToArray();
-            int tempSize = size;
-            while (tempSize < tempArr.Length)
-            {
-                tempSize *= 2;
-            }
-            this.Length = tempArr.Length;
-            mainArray = new T[tempSize];
-            //ReSize(tempArr.Length);
-            for (int i = 0; i < tempArr.Length; i++)
-            {
-                mainArray[i] = tempArr[i];
-            }
+            //int tempSize = size;
+            //while (tempSize < tempArr.Length)
+            //{
+            //    tempSize *= 2;
+            //}
+            //this.Length = tempArr.Length;
+            //mainArray = new T[tempSize];
+            ReSize(tempArr.Length);
+
+
+            //for (int i = 0; i < tempArr.Length; i++)
+            //{
+            //    mainArray[i] = tempArr[i];
+            //}
+
+            //Array.Copy(tempArr, 0, mainArray, 0, tempArr.Length);
+            Array.Copy(tempArr, mainArray, tempArr.Length);
         }
 
         public void Add(T item)
@@ -69,13 +69,20 @@ namespace MyDLL
             {
                 tempSize *= 2;
             }
+
+            //which ToArray is better?
             var tempArr = mainArray.ToArray();//Когда добвлю свой ToArray надо поменять
+            //T[] tempArr = this.ToArray();
+
             this.Length = length;
             mainArray = new T[tempSize];
-            for (int i = 0; i < tempArr.Length; i++)
-            {
-                mainArray[i] = tempArr[i];
-            }
+            //for (int i = 0; i < tempArr.Length; i++)
+            //{
+            //    mainArray[i] = tempArr[i];
+            //}
+
+            //Array.Copy(tempArr, 0, mainArray, 0, tempArr.Length);
+            Array.Copy(tempArr, mainArray, tempArr.Length);
         }
 
         public void AddRange(IEnumerable<T> sourse)
@@ -92,17 +99,42 @@ namespace MyDLL
                 {
                     Length += tempArr.Length;
                 }
-                for (int i = tempLength; i < this.Length; i++)
-                {
-                    mainArray[i] = tempArr[i - tempLength];
-                }
+                //for (int i = tempLength; i < this.Length; i++)
+                //{
+                //    mainArray[i] = tempArr[i - tempLength];
+                //}
+                Array.Copy(tempArr, 0, mainArray, tempLength, tempArr.Length);
             }            
         }
 
         public T this[int index]
         {
-            get { return mainArray[index]; }
-            set { mainArray[index] = value; }
+            get
+            {
+                index = HelperOfIndex(index);
+                return mainArray[index];
+            }
+            set
+            {
+                index = HelperOfIndex(index);
+                mainArray[index] = value;
+            }
+        }
+
+        private int HelperOfIndex(int index)
+        {
+            if (index >= Length || index < 0 - Length)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if(index >= 0)
+            {
+                return index;
+            }
+            else
+            {
+                return index + Length;
+            }
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -114,7 +146,38 @@ namespace MyDLL
         IEnumerator IEnumerable.GetEnumerator()
         {
             //return((IEnumerable<T>)mainArray).GetEnumerator();
-            return ((IEnumerable<T>)mainArray.Take(Length)).GetEnumerator();
+            //return ((IEnumerable<T>)mainArray.Take(Length)).GetEnumerator();
+            return (IEnumerator)GetEnumerator();
         }
+
+        public object Clone()
+        {
+            DynamicArray<T> arrClone = new DynamicArray<T>(this);
+            return arrClone;
+        }
+
+        public T[] ToArray()
+        {
+            T[] newArr = new T[Length];
+            Array.Copy(mainArray, newArr, Length);
+            return newArr;
+        }
+
+
+        /* Осталось
+         6. Метод Remove, удаляющий из коллекции указанный элемент. Метод должен возвращать
+            true, если удаление прошло успешно и false в противном случае. При удалении элементов
+            реальная ёмкость массива не должна уменьшаться.
+
+         7. Метод Insert, позволяющий добавить элемент в произвольную позицию массива (обратите
+            внимание, может потребоваться расширить массив). Метод должен возвращать true, если
+            добавление прошло успешно и false в противном случае. При выходе за границу массива
+            должно генерироваться исключение ArgumentOutOfRangeException.
+            
+         5. Создать новый класс: циклический динамический массив (CycledDynamicArray) на основе
+            DynamicArray, отличающийся тем, что при использовании foreach после последнего
+            элемента должен снова идти первый и так по кругу.
+         */
+
     }
 }
--- Task_3/Task_3.2/Task_3.2.1/Program.cs
+++ Task_3/Task_3.2/Task_3.2.1/Program.cs
@@ -8,11 +8,26 @@ namespace Task_3._2._1
         {
             Console.WriteLine("Hello World!");
 
-            DynamicArray<int> arr = new DynamicArray<int>(new int[]{ 1,2,3});
-            foreach (var item in arr)
-            {
-                Console.WriteLine(item);
-            }
+            DynamicArray<int> arr = new DynamicArray<int>(new int[]{ 1,2,3, 4});
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.WriteLine(string.Join("-", arr) +$"; {nameof(arr.Length)} = {arr.Length}; {nameof(arr.Capacity)} = {arr.Capacity}");
+
+            arr.Add(5);
+            Console.WriteLine(string.Join("-", arr) + $"; {nameof(arr.Length)} = {arr.Length}; {nameof(arr.Capacity)} = {arr.Capacity}");
+
+            arr.AddRange(new[] { 6, 7, 8, 9, 10 });
+            Console.WriteLine(string.Join("-", arr) + $"; {nameof(arr.Length)} = {arr.Length}; {nameof(arr.Capacity)} = {arr.Capacity}");
+
+            DynamicArray<int> arr1 = (DynamicArray<int>)arr.Clone();
+            Console.WriteLine(string.Join("-", arr1) + $"; {nameof(arr1.Length)} = {arr1.Length}; {nameof(arr1.Capacity)} = {arr1.Capacity}");
+
+            int[] arr2 = arr1.ToArray();
+            Console.WriteLine(string.Join("-", arr2) + $"; {nameof(arr2.Length)} = {arr2.Length}");
+
             Console.ReadLine();
         }
     }
