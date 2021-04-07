d4d396adc28b65727d9b6f36e98161fef44152c1
shigontsev <shigontsev1@mail.ru>
2021-04-08 00:35:32 +0400

    add task 3.3.2

---
+++ Task_3/Task_3.3/Task_3.3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_3._3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var text = new {
+                digital = "2388378289",
+                rus = "ЕхалоуХьюман",
+                eng = "HelloHuman",
+                mixed = "Hello android №085"
+            };
+
+            Console.WriteLine(text.digital.GetTypeText());
+
+            Console.WriteLine(text.rus.GetTypeText());
+
+            Console.WriteLine(text.eng.GetTypeText());
+
+            Console.WriteLine(text.mixed.GetTypeText());
+
+            Console.ReadLine();
+        }
+    }
+}
