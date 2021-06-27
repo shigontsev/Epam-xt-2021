f2c465296a9479bf0de435f9799d733c4a6f1390
shigontsev <shigontsev1@mail.ru>
2021-06-27 21:13:54 +0400

    expanded Task_8.1, add pages

--- Task_8/Task_8.JsonDAL/FilePath.cs
+++ Task_8/Task_8.JsonDAL/FilePath.cs
@@ -22,7 +22,9 @@ namespace Task_8.JsonDAL
 
         //public const string JsonUsersAndAwardsPath = "UsersAndAwards.json";
 
-        private const string JsonFilesPath = @"App_Data\";
+        //private const string JsonFilesPath = @"App_Data\";
+
+        private const string JsonFilesPath = @"E:\Repos\Epam-xt-2021\Task_8\Task_8.PL.WebPages\App_Data\";
 
         public const string JsonUsersPath = JsonFilesPath + "Users.json";
 
